4e02f1b6e483f213ec3664373e9a09efd62baf47
AJ <ashishshetty2612@gmail.com>
2021-07-15 23:24:52 +0530

    mobile view style fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { HamburgerIcon } from "../../assets/icons";
 import { useAuth } from "../../context/auth-context";
@@ -6,6 +6,7 @@ import "./header.css";
 
 export function Header() {
   const { user, logout } = useAuth();
+  const [showMenu, setShowMenu] = useState(false);
   const navigate = useNavigate();
   const { pathname } = useLocation();
   async function authBtnHandler() {
@@ -17,6 +18,9 @@ export function Header() {
       navigate("/login", { state: { form: pathname } });
     }
   }
+  let classname = showMenu
+    ? "menuDrawer menuDrawerActive "
+    : "menuDrawerHidden";
   return (
     <>
       <nav className="nav">
@@ -39,11 +43,34 @@ export function Header() {
             </span>
           </ul>
         </div>
+        <div className="mobile__nav">
+          <button
+            className="btn__user--action hamburger"
+            onClick={() => setShowMenu((showMenu) => !showMenu)}
+          >
+            <HamburgerIcon />
+          </button>
+          <div
+            className={classname}
+            onClick={() => setShowMenu((showMenu) => !showMenu)}
+          >
+            <ul className="flex flex-coloumn ">
+              <NavLink className="header_nav__link " to="/">
+                Home
+              </NavLink>
+              <NavLink className="header_nav__link" to="/playlist">
+                Playlist
+              </NavLink>
+              <NavLink className="header_nav__link" to="/account">
+                Account
+              </NavLink>
+              <span className="header_nav__link " onClick={authBtnHandler}>
+                {user ? "Logout" : "Login"}
+              </span>
+            </ul>
+          </div>
+        </div>
       </nav>
-      <div className="mobile__nav">
-        <HamburgerIcon />
-        Menu
-      </div>
     </>
   );
 }
